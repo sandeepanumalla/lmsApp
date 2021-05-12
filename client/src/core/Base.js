4663ae94bc8252c1c1f77ffa49131f4689d0251e
Sandeep Anumalla <42109850+sandeepanumalla@users.noreply.github.com>
2021-05-12 19:23:46 +0530

    Update Base.js

--- client/src/core/Base.js
+++ client/src/core/Base.js
@@ -10,7 +10,7 @@ const Base = ({
 }) => (
   <div>
   <Menu />
-    <div style={{height:'80vh'}} className="container-fluid bg-light">
+    <div  className="container-fluid bg-light">
       <div className="jumbotron bg-light text-dark text-center">
         <h2 className="display-4">{title}</h2>
         <p className="lead">{description}</p>
